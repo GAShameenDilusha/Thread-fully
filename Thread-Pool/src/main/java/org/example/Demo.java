3eb66b2f1e534c08a0f938e15e4082cfc2b58ce9
shameen <shameendilusha5678@gmail.com>
2024-04-22 14:01:21 +0530

    add InterThread.java and Thread-Pool

---
+++ Thread-Pool/src/main/java/org/example/Demo.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Demo {
+    public static void main(String[] args) {
+        ExecutorService executorService= Executors.newFixedThreadPool(5);   //Methana dena count eka anuwa thama thread pool ekata ganne...
+        for (int i=0;i<10;i++){
+            Runnable runnable = new MyRunnable("workerThread :" + i);
+            executorService.execute(runnable);
+        }
+        executorService.shutdown();
+        while (executorService.isTerminated()){}
+        System.out.println("success");
+    }
+
+
+}
